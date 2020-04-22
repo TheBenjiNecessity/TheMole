052b28e677769bf72db1a33e4df5536da2637fee
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-04-21 23:12:59 -0400

    Add game route

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/home';
+import { Game } from './pages/game';
 import { NoMatch } from './pages/nomatch';
+
+import './App.scss';
 
 export class Routes extends Component {
     render() {
         return (
             <Switch>
+                <Route path="/Game" component={Game} />
                 <Route exact path="/" component={Home} />
                 <Route component={NoMatch} />
             </Switch>
---
+++ src/pages/game/Game.js
@@ -0,0 +1,32 @@
+import React, { Component } from './node_modules/react';
+
+import './game.scss';
+import { NavBar } from '../../components/navbar';
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+
+        this.play = this.play.bind(this);
+
+        this.state = { name: '', roomcode: '' };
+    }
+
+    play() {
+
+    }
+
+    render() {
+
+        return  (
+            <div className="main">
+                <NavBar title="The Mole"></NavBar>
+                <div className="panel centered-panel centered-panel-medium">
+                    test
+                </div>
+            </div>
+        );
+    };
+}
+
+export default Game;
