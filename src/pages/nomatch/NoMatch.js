b6d75be97ef38cf107595cd22421892d8affca92
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-04-21 23:12:38 -0400

    Populate nomatch route

--- src/pages/nomatch/NoMatch.js
+++ src/pages/nomatch/NoMatch.js
@@ -1,7 +1,22 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { NavBar } from '../../components/navbar';
 
 class NoMatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
 
+    render() {
+        return  (
+            <div className="main">
+                <NavBar title="The Mole"></NavBar>
+                <div className="panel centered-panel centered-panel-medium">
+                    No Match
+                </div>
+            </div>
+        );
+    }
 }
 
 export default NoMatch;
