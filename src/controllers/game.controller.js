a3697022b3c49852fc23aabebac713e6fcee1700
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2020-05-31 16:12:43 -0400

    Add getters/setters

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -1,9 +1,14 @@
 import SocketService from '../services/socket.service';
 
+const MOLEROOMSTORAGEKEY = 'mole-room';
+const MOLEPLAYERSTORAGEKEY = 'mole-player';
+const MOLEWSURLSTORAGEKEY = 'mole-ws-url';
+
 class GameController {
 	player = null;
 	room = null;
 	isHost = false;
+	wsUrl = null;
 
 	constructor() {
 		if (!GameController.shared) {
@@ -30,10 +35,45 @@ class GameController {
 
 	setCurrentPlayer(player) {
 		this.player = player;
+		window.localStorage.setItem(MOLEPLAYERSTORAGEKEY, JSON.stringify(this.player));
+	}
+
+	getCurrentPlayer() {
+		if (!this.player) {
+			this.player = JSON.parse(window.localStorage.getItem(MOLEPLAYERSTORAGEKEY));
+		}
+
+		return this.player;
 	}
 
 	setRoom(room) {
 		this.room = room;
+		window.localStorage.setItem(MOLEROOMSTORAGEKEY, JSON.stringify(this.room));
+	}
+
+	getRoom() {
+		if (!this.room) {
+			this.room = JSON.parse(window.localStorage.getItem(MOLEROOMSTORAGEKEY));
+		}
+
+		return this.room;
+	}
+
+	setWSUrl(url) {
+		this.wsUrl = url;
+		window.localStorage.setItem(MOLEWSURLSTORAGEKEY, this.wsUrl);
+	}
+
+	getWSUrl() {
+		if (!this.wsUrl) {
+			this.wsUrl = window.localStorage.getItem(MOLEWSURLSTORAGEKEY);
+		}
+
+		return this.wsUrl;
+	}
+
+	startPlay() {
+		SocketService.sendMessageToRoom('start-game', this.room.roomcode);
 	}
 
 	// onMessage(obj) {
