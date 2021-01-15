b1e004974dcfc598f1cefaf216fa2aec378c9488
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-01-15 11:56:36 -0500

    Fix styles

--- src/common/NextPanel/index.jsx
+++ src/common/NextPanel/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import storageService from '../../services/storage.service';
 
@@ -6,21 +6,21 @@ import './next-panel.scss';
 
 const NextPanel = ({ titleText, nextButtonText, onNext, children }) => {
 	return (
-		<div className="panel centered-panel centered-panel-medium next-panel">
-			<h1 className="title col-sm-12">{titleText}</h1>
-			<hr />
-			{children}
+		<Fragment>
+			<div className="panel centered-panel centered-panel-medium next-panel">
+				<h1 className="title col-sm-12">{titleText}</h1>
+				<hr />
+				{children}
+			</div>
 			{!storageService.isHost() && (
-				<div class="panel-bottom">
+				<div className="panel centered-panel centered-panel-medium bottom-panel">
 					<hr />
-					<div>
-						<button type="button" className="button button-primary" onClick={onNext}>
-							{nextButtonText}
-						</button>
-					</div>
+					<button type="button" className="button button-primary centered" onClick={onNext}>
+						{nextButtonText}
+					</button>
 				</div>
 			)}
-		</div>
+		</Fragment>
 	);
 };
 
