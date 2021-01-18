4d034b3ad84a7c56386db84aac183ba387b5ee94
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-01-18 08:53:36 -0500

    Hide text when null

--- src/common/NextPanel/index.jsx
+++ src/common/NextPanel/index.jsx
@@ -8,15 +8,19 @@ const NextPanel = ({ titleText, nextButtonText, onNext, children }) => {
 	return (
 		<Fragment>
 			<div className="panel centered-panel centered-panel-medium next-panel">
-				<h1 className="title col-sm-12">{titleText}</h1>
-				<hr />
+				{titleText && (
+					<Fragment>
+						<h1 className="title col-sm-12">{titleText}</h1>
+						<hr />
+					</Fragment>
+				)}
 				{children}
 			</div>
 			{!storageService.isHost() && (
 				<div className="panel centered-panel centered-panel-medium bottom-panel">
 					<hr />
 					<button type="button" className="button button-primary centered" onClick={onNext}>
-						{nextButtonText}
+						{nextButtonText ? nextButtonText : 'Next'}
 					</button>
 				</div>
 			)}
