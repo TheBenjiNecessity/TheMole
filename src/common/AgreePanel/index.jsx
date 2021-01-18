28f4d31db7553542c45f69779de41507431b2e1a
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-01-18 08:53:02 -0500

    Add agree panel component

---
+++ src/common/AgreePanel/index.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+import storageService from '../../services/storage.service';
+
+import './agree-panel.scss';
+
+const AgreePanel = ({ room }) => {
+	return null;
+};
+
+export default AgreePanel;
